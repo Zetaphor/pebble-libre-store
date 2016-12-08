64c0827fb479c8ce3870bec157854629abb51d62
Gary Daigle <zetaphor@gmail.com>
2016-12-08 17:28:42 -0500

    Added more examples to watchfaces component

--- src/components/pages/Watchfaces.vue
+++ src/components/pages/Watchfaces.vue
@@ -38,6 +38,45 @@
         </div>
       </div>
     </div>
+
+    <div class="col-xs-4">
+      <div class="card">
+        <div class="card-block">
+          <p class="card-title text-xs-center">Example Watchface</p>
+          <img class="img-fluid mx-auto d-block preview-image" src="https://assets.getpebble.com/api/file/Wn2zo34FTwuUw2lAEHVl/convert?cache=true&fit=crop&w=144&h=168" />
+          <p class="card-text text-xs-center">
+            <span class="icon-heart"></span>
+            <span class="total-hearts">193</span>
+          </p>
+        </div>
+      </div>
+    </div>
+
+    <div class="col-xs-4">
+      <div class="card">
+        <div class="card-block">
+          <p class="card-title text-xs-center">Example Watchface</p>
+          <img class="img-fluid mx-auto d-block preview-image" src="https://assets.getpebble.com/api/file/Wn2zo34FTwuUw2lAEHVl/convert?cache=true&fit=crop&w=144&h=168" />
+          <p class="card-text text-xs-center">
+            <span class="icon-heart"></span>
+            <span class="total-hearts">193</span>
+          </p>
+        </div>
+      </div>
+    </div>
+
+    <div class="col-xs-4">
+      <div class="card">
+        <div class="card-block">
+          <p class="card-title text-xs-center">Example Watchface</p>
+          <img class="img-fluid mx-auto d-block preview-image" src="https://assets.getpebble.com/api/file/Wn2zo34FTwuUw2lAEHVl/convert?cache=true&fit=crop&w=144&h=168" />
+          <p class="card-text text-xs-center">
+            <span class="icon-heart"></span>
+            <span class="total-hearts">193</span>
+          </p>
+        </div>
+      </div>
+    </div>
 </template>
 
 <script>
