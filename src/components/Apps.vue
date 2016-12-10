0c03f7a41d9207c9e460d1ef1c305528da2745f9
Gary Daigle <zetaphor@gmail.com>
2016-12-10 03:13:16 -0500

    Apps page is complete

---
+++ src/components/Apps.vue
@@ -0,0 +1,326 @@
+<template>
+  <div class="apps col-xs-9 offset-xs-2">
+    <div class="category-menu">
+      <div class="row">
+        <div class="col-xs-6 app-category">
+          <img class="category-image float-xs-left" src="../assets/category-daily.png" />
+          <p class="color-daily">Daily</p>
+        </div>
+
+        <div class="col-xs-6 app-category">
+          <img class="category-image float-xs-left" src="../assets/category-tools.png" />
+          <p class="color-tools">Tools & Utilities</p>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-xs-6 app-category">
+          <img class="category-image float-xs-left" src="../assets/category-notifications.png" />
+          <p class="color-notifications">Notifications</p>
+        </div>
+
+        <div class="col-xs-6 app-category">
+          <img class="category-image float-xs-left" src="../assets/category-remotes.png" />
+          <p class="color-remotes">Remotes</p>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-xs-6 app-category">
+          <img class="category-image float-xs-left" src="../assets/category-health.png" />
+          <p class="color-health">Health & Fitness</p>
+        </div>
+
+        <div class="col-xs-6 app-category">
+          <img class="category-image float-xs-left" src="../assets/category-games.png" />
+          <p class="color-games">Games</p>
+        </div>
+      </div>
+    </div>
+
+    <div class="app-collection clearfix">
+      <p class="collection-title">Fresh Picks</p>
+
+      <div class="collection-blocks col-xs-10 offset-xs-1">
+        <div class="row">
+          <div class="col-xs-4">
+            <div class="card">
+              <div class="card-block app-block">
+                <img class="img-fluid float-xs-left preview-image" src="https://assets.getpebble.com/api/file/S1nCXN4uSLyO0jGGxVto/convert?cache=true&fit=crop&w=80&h=80" />
+                <p class="card-title">Example App</p>
+                <p class="card-category color-games">Games</p>
+                <p class="card-text">
+                  <span class="icon-heart"></span>
+                  <span class="total-hearts">193</span>
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-xs-4">
+            <div class="card">
+              <div class="card-block app-block">
+                <img class="img-fluid float-xs-left preview-image" src="https://assets.getpebble.com/api/file/S1nCXN4uSLyO0jGGxVto/convert?cache=true&fit=crop&w=80&h=80" />
+                <p class="card-title">Example App</p>
+                <p class="card-category color-games">Games</p>
+                <p class="card-text">
+                  <span class="icon-heart"></span>
+                  <span class="total-hearts">193</span>
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-xs-4">
+            <div class="card">
+              <div class="card-block app-block">
+                <img class="img-fluid float-xs-left preview-image" src="https://assets.getpebble.com/api/file/S1nCXN4uSLyO0jGGxVto/convert?cache=true&fit=crop&w=80&h=80" />
+                <p class="card-title">Example App</p>
+                <p class="card-category color-games">Games</p>
+                <p class="card-text">
+                  <span class="icon-heart"></span>
+                  <span class="total-hearts">193</span>
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-xs-4">
+            <div class="card">
+              <div class="card-block app-block">
+                <img class="img-fluid float-xs-left preview-image" src="https://assets.getpebble.com/api/file/S1nCXN4uSLyO0jGGxVto/convert?cache=true&fit=crop&w=80&h=80" />
+                <p class="card-title">Example App</p>
+                <p class="card-category color-games">Games</p>
+                <p class="card-text">
+                  <span class="icon-heart"></span>
+                  <span class="total-hearts">193</span>
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-xs-4">
+            <div class="card">
+              <div class="card-block app-block">
+                <img class="img-fluid float-xs-left preview-image" src="https://assets.getpebble.com/api/file/S1nCXN4uSLyO0jGGxVto/convert?cache=true&fit=crop&w=80&h=80" />
+                <p class="card-title">Example App</p>
+                <p class="card-category color-games">Games</p>
+                <p class="card-text">
+                  <span class="icon-heart"></span>
+                  <span class="total-hearts">193</span>
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-xs-4">
+            <div class="card">
+              <div class="card-block app-block">
+                <img class="img-fluid float-xs-left preview-image" src="https://assets.getpebble.com/api/file/S1nCXN4uSLyO0jGGxVto/convert?cache=true&fit=crop&w=80&h=80" />
+                <p class="card-title">Example App</p>
+                <p class="card-category color-games">Games</p>
+                <p class="card-text">
+                  <span class="icon-heart"></span>
+                  <span class="total-hearts">193</span>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="app-collection clearfix">
+      <p class="collection-title">Recently Added</p>
+
+      <div class="collection-blocks col-xs-10 offset-xs-1">
+        <div class="row">
+          <div class="col-xs-4">
+            <div class="card">
+              <div class="card-block app-block">
+                <img class="img-fluid float-xs-left preview-image" src="https://assets.getpebble.com/api/file/S1nCXN4uSLyO0jGGxVto/convert?cache=true&fit=crop&w=80&h=80" />
+                <p class="card-title">Example App</p>
+                <p class="card-category color-games">Games</p>
+                <p class="card-text">
+                  <span class="icon-heart"></span>
+                  <span class="total-hearts">193</span>
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-xs-4">
+            <div class="card">
+              <div class="card-block app-block">
+                <img class="img-fluid float-xs-left preview-image" src="https://assets.getpebble.com/api/file/S1nCXN4uSLyO0jGGxVto/convert?cache=true&fit=crop&w=80&h=80" />
+                <p class="card-title">Example App</p>
+                <p class="card-category color-games">Games</p>
+                <p class="card-text">
+                  <span class="icon-heart"></span>
+                  <span class="total-hearts">193</span>
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-xs-4">
+            <div class="card">
+              <div class="card-block app-block">
+                <img class="img-fluid float-xs-left preview-image" src="https://assets.getpebble.com/api/file/S1nCXN4uSLyO0jGGxVto/convert?cache=true&fit=crop&w=80&h=80" />
+                <p class="card-title">Example App</p>
+                <p class="card-category color-games">Games</p>
+                <p class="card-text">
+                  <span class="icon-heart"></span>
+                  <span class="total-hearts">193</span>
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-xs-4">
+            <div class="card">
+              <div class="card-block app-block">
+                <img class="img-fluid float-xs-left preview-image" src="https://assets.getpebble.com/api/file/S1nCXN4uSLyO0jGGxVto/convert?cache=true&fit=crop&w=80&h=80" />
+                <p class="card-title">Example App</p>
+                <p class="card-category color-games">Games</p>
+                <p class="card-text">
+                  <span class="icon-heart"></span>
+                  <span class="total-hearts">193</span>
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-xs-4">
+            <div class="card">
+              <div class="card-block app-block">
+                <img class="img-fluid float-xs-left preview-image" src="https://assets.getpebble.com/api/file/S1nCXN4uSLyO0jGGxVto/convert?cache=true&fit=crop&w=80&h=80" />
+                <p class="card-title">Example App</p>
+                <p class="card-category color-games">Games</p>
+                <p class="card-text">
+                  <span class="icon-heart"></span>
+                  <span class="total-hearts">193</span>
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-xs-4">
+            <div class="card">
+              <div class="card-block app-block">
+                <img class="img-fluid float-xs-left preview-image" src="https://assets.getpebble.com/api/file/S1nCXN4uSLyO0jGGxVto/convert?cache=true&fit=crop&w=80&h=80" />
+                <p class="card-title">Example App</p>
+                <p class="card-category color-games">Games</p>
+                <p class="card-text">
+                  <span class="icon-heart"></span>
+                  <span class="total-hearts">193</span>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'apps'
+  }
+</script>
+
+<style lang="scss" scoped>
+  $color-daily: #3db9e6;
+  $color-tools: #fdbf37;
+  $color-notifications: #FF9000;
+  $color-remotes: #fc4b4b;
+  $color-health: #98D500;
+  $color-games: #b57ad3;
+
+  $pebbleMedGrey: #484848;
+  $pebbleMedDarkGrey: #333333;
+  $pebbleOrange: #ff4700;
+  $pebbleWhite: #F4F3F4;
+
+  .color-daily { color: $color-daily; }
+  .color-tools { color: $color-tools; }
+  .color-notifications { color: $color-notifications; }
+  .color-remotes { color: $color-remotes; }
+  .color-health { color: $color-health; }
+  .color-games { color: $color-games; }
+
+  .apps {
+    background-color: $pebbleWhite;
+    border-radius: 0 0 10px 10px;
+    padding: 0;
+  }
+
+  .category-menu {
+    border-top: 2px solid $pebbleOrange;
+    border-bottom: 2px solid $pebbleOrange;
+    background-color: $pebbleMedDarkGrey;
+
+    .row {
+      margin: 0;
+    }
+
+    .app-category {
+      border-top: 1px solid $pebbleMedGrey;
+      padding-top: 1rem;
+        img {
+          width: 34px;
+          height: auto;
+        }
+
+        p {
+          padding-top: 10px;
+          margin-left: 40px;
+          text-transform: uppercase;
+        }
+    }
+  }
+
+  .app-collection {
+    .card {
+      border: none !important;
+      font-size: 16px;
+      line-height: 1.3rem;
+
+
+      .card-title {
+        font-size: 18px;
+        white-space: nowrap;
+        text-overflow: ellipsis;
+        overflow: hidden;
+        padding-left: 11px;
+      }
+    }
+
+    .collection-title {
+      padding: 0 0 0.5rem 2rem;
+      margin: 1rem 0 1rem 0;
+      border-bottom: 1px solid #dbd9d8;
+      color: $pebbleOrange;
+      text-transform: uppercase;
+      font-size: 14px;
+    }
+
+    .collection-block {
+      padding-left: 1rem;
+    }
+
+    .app-block {
+      padding: 1rem;
+      img {
+        width: 64px;
+        height: auto;
+      }
+
+      p {
+        margin: 0;
+        padding-left: 74px;
+      }
+    }
+  }
+</style>
